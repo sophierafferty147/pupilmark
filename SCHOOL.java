759c959fccd89f55ec053f843727d28109e494f8
sophierafferty147 <Guest@stk-cs01-ws14.westlothian.sch.uk>
2015-11-30 14:06:15 +0000

    sprint 2.1

---
+++ SCHOOL.java
@@ -0,0 +1,33 @@
+
+
+import javax.swing.JOptionPane;
+import java.io.*;
+public class SCHOOL
+{
+    // array of PUPIL objects
+    private PUPIL pupilList[]; 
+    // number of pupils to be called
+    int noOfPupils;
+
+    public SCHOOL()
+    {
+
+    }
+
+    // top level algorithm
+    public void processpupils()
+    { 
+        setupPupilList();
+        countpupilmark();
+
+    }
+
+    private void setupPupilList()
+    { // placeholder
+        
+    }
+    public void countpupilmark()
+    {
+        //placeholder
+    }
+}
