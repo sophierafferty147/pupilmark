0cb42386a53489380aab7ce9fc8b30306b68a969
sophierafferty147 <Guest@stk-cs01-ws14.westlothian.sch.uk>
2015-11-30 14:57:26 +0000

    sprint 2.4
    not finished

--- SCHOOL.java
+++ SCHOOL.java
@@ -8,24 +8,33 @@ public class SCHOOL
     private PUPIL pupilList[]; 
     // number of pupils to be called
     int noOfPupils;
+    FILEREADCSV pupilFile;
 
     public SCHOOL()
     {
-
+        pupilFile = new FILEREADCSV();
     }
 
     // top level algorithm
-    public void processpupils()
+    public void processpupils() throws IOException
     { 
         setupPupilList();
         countpupilmark();
-
     }
 
-    private void setupPupilList()
-    { // placeholder
-        
+    private void setupPupilList() throws IOException
+    { 
+        // first user member
+        System.out.println("School: Pupil mark update");
+        System.out.println("** Preparing to read data file");
+        // read file, fetch data as String array containing the rows
+        String[] dataRows = pupilmark.readCSVtable();
+        // calculate the number of member rows, skip headings
+        noOfPupils = dataRows.length - 1;
+        // update user with number of rows with membership details
+        System.out.println ("** " + noOfPupils + " rows read.\n\n");
     }
+    
     public void countpupilmark()
     {
         //placeholder
