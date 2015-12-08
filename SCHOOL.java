5cc845ff490b2f3b8206dea868b5d69c8f8462aa
sophierafferty147 <Guest@stk-cs01-ws14.westlothian.sch.uk>
2015-12-08 10:34:28 +0000

    sprint 5

--- SCHOOL.java
+++ SCHOOL.java
@@ -8,11 +8,13 @@ public class SCHOOL
     // number of pupils to be called
     int noOfPupils;
     FILEREADCSV pupilmark;
+    FILEWRITECSV resultFile;
     private int topmark;
     private int maxDataPosition;
     public SCHOOL()
     {
         pupilmark = new FILEREADCSV();
+        resultFile = new FILEWRITECSV();
     }
 
     // top level algorithm
@@ -30,19 +32,21 @@ public class SCHOOL
         // read file, fetch data as String array containing the rows
         String[] dataRows = pupilmark.readCSVtable ();
         // calculate the number of member rows, skip headings
-        noOfPupils = dataRows.length - 1;
+        noOfPupils = dataRows.length;
         // update user with number of rows with pupil details
         System.out.println ("** " + noOfPupils + " rows read.\n\n");
         pupilList = new PUPIL[noOfPupils];
         for (int i = 0; i < noOfPupils; i++)
         {
             pupilList[i] = new PUPIL();
-            pupilList[i].readPupilDetails(dataRows[i+1]);
+            pupilList[i].readPupilDetails(dataRows[i]);
         }
     }
 
-    public void findmaxData(){
+    public void findmaxData() throws IOException
+    {
        int maxDataposition = 0;
+       String fileContent = "";
         for (int i = 1; i < noOfPupils; i++)
         {
             if (pupilList[i].getpupilmark() > pupilList [maxDataPosition].getpupilmark())
@@ -50,8 +54,18 @@ public class SCHOOL
             maxDataPosition = i;
         }
       }
+      if (maxDataPosition>1)
+      {
+          fileContent = fileContent.concat("\n");
+        }
+      fileContent = fileContent.concat(pupilList[1].writeDetails());
+      
       System.out.print("Top Mark is:" + maxDataPosition);
       pupilList[maxDataPosition].displaydetails();
       System.out.println();
+      
+      System.out.println("** Preparing to write data file.");
+      resultFile.writeCSVtable(fileContent);
+      System.out.println("** File written and closed.");
      }
     }
